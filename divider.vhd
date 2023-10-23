5dc9dc24d5cbb3db6c1200358c7edcb0e9524e93
Gyda Nawarungruang <111736115+gydanawa@users.noreply.github.com>
2023-10-23 16:58:03 -0500

    Add files via upload
    Added clock into testbench and edited file reading to consider start and clock signals.
    Compiles successfully but inputs to dinl_temp are not occurring.
    Fix testbench first though.

--- divider.vhd
+++ divider.vhd
@@ -7,8 +7,8 @@ USE IEEE.std_logic_textio.ALL;
 
 ENTITY divider IS
     PORT (
+        --Inputs
         clk : IN STD_LOGIC;
-        --COMMENT OUT clk signal for Part A.
         start : IN STD_LOGIC;
         dividend : IN STD_LOGIC_VECTOR (DIVIDEND_WIDTH - 1 DOWNTO 0);
         divisor : IN STD_LOGIC_VECTOR (DIVISOR_WIDTH - 1 DOWNTO 0);
@@ -40,67 +40,126 @@ ARCHITECTURE behavioral_sequential OF divider IS
     SIGNAL DINL_var : DINL_var_type;
     TYPE DOUT_var_type IS ARRAY(DIVIDEND_WIDTH - 1 DOWNTO 0) OF STD_LOGIC_VECTOR(DIVISOR_WIDTH - 1 DOWNTO 0);
     SIGNAL DOUT_var : DOUT_var_type;
-    SIGNAL temp_dinl : STD_LOGIC_VECTOR (DIVISOR_WIDTH DOWNTO 0);
+
+    --signals for clocking (storing new vs old inputs)
+    SIGNAL dividend_temp : STD_LOGIC_VECTOR(DIVIDEND_WIDTH - 1 DOWNTO 0);
+    SIGNAL dinl_temp : STD_LOGIC_VECTOR (DIVISOR_WIDTH DOWNTO 0);
+    SIGNAL divisor_temp : STD_LOGIC_VECTOR(DIVISOR_WIDTH - 1 DOWNTO 0);
+    SIGNAL dout_temp : STD_LOGIC_VECTOR(DIVISOR_WIDTH - 1 DOWNTO 0);
+    SIGNAL i : INTEGER := 0;
+
+    --signals for temporary outputs
     SIGNAL quotient_temp : STD_LOGIC_VECTOR (DIVIDEND_WIDTH - 1 DOWNTO 0);
-   -- SIGNAL remainder_temp : STD_LOGIC_VECTOR (DIVISOR_WIDTH - 1 DOWNTO 0);
+    SIGNAL quotient_bit : STD_LOGIC;
     SIGNAL overflow_temp : STD_LOGIC;
+    SIGNAL remainder_temp : STD_LOGIC_VECTOR (DIVISOR_WIDTH - 1 DOWNTO 0);
+
+
 
 BEGIN
-    seq_divide : PROCESS (clk, start) BEGIN
-        --compute overflow_temp
-        IF  (to_integer(unsigned(divisor)) = 0) then
-            overflow_temp <= '1';
-        ELSE
-            overflow_temp <= '0';
-        END IF;
+    --initialize division index variable
+    -- i <= 0; 
 
-        --do actual division
-        divide_loop : FOR i IN 0 TO DIVIDEND_WIDTH - 1 LOOP
-            --start loop
-            --feed inputs into comparator 
-            --do one step division
-            --feed inputs back into comparator (instantly?)
-            --comparator only starts on the rising clock edge
+    --instantiate a single comparator
+    comp_first : comparator
+    PORT MAP(
+        DINL => dinl_temp,
+        DINR => divisor_temp,
+        DOUT => dout_temp,
+        isGreaterEq => quotient_bit
+        -- isGreaterEq => quotient_temp(DIVIDEND_WIDTH - 1 - i)
+    );
 
-            --figure out how to change temp_dinl according to current case
-            --prepare inputs for the next clock cycle
-
-            CASE (i) IS
-                WHEN (0)
-                    temp_dinl <= (0 => dividend(DIVIDEND_WIDTH - 1), OTHERS => '0');
-                WHEN (1 TO DIVIDEND_WIDTH - 1)
-                    temp_din1 <= DINL_var(i - 1);
-            END CASE;
-
-            WHILE (!rising_edge(clk)) LOOP
-                --wait for rising clock edge
-            END LOOP;
-
-            IF (rising_edge(clk)) THEN
-                comp_first : comparator
-                PORT MAP(-- not necessarily concurrent
-                    DINL => temp_dinl,
-                    DINR => divisor,
-                    DOUT => DOUT_var(i),
-                    isGreaterEq => quotient_temp(DIVIDEND_WIDTH - 1 - i)
-                );
-                DINL_var(i) <= <= DOUT_var(i) & dividend(DIVIDEND_WIDTH - 2 - i);
-
-            END IF;
-        END LOOP divide_loop;
-
-        --release results in temp variables to actual result variables
-        --concurrently assign signals outside to prevent delay
+    --perform the actual division
+    seq_divide : PROCESS (clk, start)
+    BEGIN
         IF (rising_edge(start)) THEN
-            quotient <= quotient_temp;
-            remainder <= DOUT_var(DIVIDEND_WIDTH - 1);
-            overflow <= overflow_temp;
-        END IF;
+            --update new inputs
+            dividend_temp <= dividend;
+            divisor_temp <= divisor;
 
+            --set dividend index to 0
+            i <= 0;
+
+            --set up first comparator inputs
+            dinl_temp <= (0 => dividend_temp(DIVIDEND_WIDTH - 1), OTHERS => '0');
+
+            --compute overflow_temp
+            IF (to_integer(unsigned(divisor_temp)) = 0) THEN
+                overflow_temp <= '1';
+            ELSE
+                overflow_temp <= '0';
+            END IF;
+
+            
+
+        --check for clock edge
+        ELSIF (rising_edge(clk)) THEN
+            IF (i < DIVIDEND_WIDTH) THEN
+                --store IsGreaterEq result 
+                quotient_temp(DIVIDEND_WIDTH - 1 - i) <= quotient_bit;
+                --update din1_temp with previous output
+                dinl_temp <= dout_temp & dividend_temp(DIVIDEND_WIDTH - 2 - i);
+                --increment division index
+                i <= i + 1;
+            ELSE 
+                --done with division, so assign remainder as last dout
+                remainder_temp <= dout_temp;
+            END IF;
+        END IF;
     END PROCESS seq_divide;
 
-END ARCHITECTURE behavioral_sequential;
+    --concurrently assign output outside of process to prevent delay
+    quotient <= quotient_temp;
+    remainder <= remainder_temp;
+    overflow <= overflow_temp;
 
+END ARCHITECTURE behavioral_sequential;
+--         --DINL_var(i) <= DOUT_var(i) & dividend_temp(DIVIDEND_WIDTH - 2 - i);
+
+--         divide_loop : FOR i IN 0 TO DIVIDEND_WIDTH - 1 LOOP
+--             --start loop
+--             --feed inputs into comparator 
+--             --do one step division
+--             --feed inputs back into comparator (instantly?)
+--             --comparator only starts on the rising clock edge
+
+--             --figure out how to change temp_dinl according to current case
+--             --prepare inputs for the next clock cycle
+
+--             CASE (i) IS
+--                 WHEN (0)
+--                     temp_dinl <= (0 => dividend_temp(DIVIDEND_WIDTH - 1), OTHERS => '0');
+--                 WHEN (1 TO DIVIDEND_WIDTH - 1)
+--                     temp_din1 <= DINL_var(i - 1);
+--             END CASE;
+
+--             WHILE (NOT rising_edge(clk)) LOOP
+--                 --wait for rising clock edge
+--             END LOOP;
+
+--             IF (rising_edge(clk)) THEN
+--                 comp_first : comparator
+--                 PORT MAP(-- not necessarily concurrent
+--                     DINL => temp_dinl,
+--                     DINR => divisor_temp,
+--                     DOUT => DOUT_var(i),
+--                     isGreaterEq => quotient_temp(DIVIDEND_WIDTH - 1 - i)
+--                 );
+--                 DINL_var(i) <= DOUT_var(i) & dividend_temp(DIVIDEND_WIDTH - 2 - i);
+
+--             END IF;
+--         END LOOP;
+
+--     END IF;
+--     -- end process seq_divide;
+--     --release results in temp variables to actual result variables
+--     --concurrently assign signals outside to prevent delay
+--     IF (rising_edge(start)) THEN
+--         quotient <= quotient_temp;
+--         remainder <= DOUT_var(DIVIDEND_WIDTH - 1);
+--         overflow <= overflow_temp;
+--     END IF;
 ARCHITECTURE structural_combinational OF divider IS
     --Signals and components go here
     COMPONENT comparator IS
@@ -166,7 +225,7 @@ BEGIN
                 DINL => DINL_var(i - 1), --t_in3,
                 DINR => divisor,
                 DOUT => remainder_temp,
-                isGreaterEq => quotient_temp(DIVIDEND_WIDTH - 1 - i) -- ie last bit
+                isGreaterEq => quotient_temp(DIVIDEND_WIDTH - 1 - i) -- i.e. last bit
             );
         END GENERATE lastslice;
 
--- divider_tb.vhd
+++ divider_tb.vhd
@@ -27,8 +27,9 @@ ARCHITECTURE behavioral OF testbench1 IS
     --specify which architecture to use
     for all : divider use entity WORK.divider (behavioral_sequential);
 
-
+    --declare testbench signals
     SIGNAL clk_tb : STD_LOGIC := '0';
+    constant PERIOD : time := 10 ns;
     SIGNAL start_tb : STD_LOGIC := '0';
     SIGNAL dividend_tb : STD_LOGIC_VECTOR (DIVIDEND_WIDTH - 1 DOWNTO 0) := (OTHERS => '0'); -- dividend
     SIGNAL divisor_tb : STD_LOGIC_VECTOR (DIVISOR_WIDTH - 1 DOWNTO 0) := (OTHERS => '0'); -- divsor
@@ -49,47 +50,65 @@ BEGIN
         overflow => overflow_tb
     );
 
+    --instantiate clock
+    clk_generate: process is
+        begin
+            clk_tb <= '0';
+            wait for (PERIOD/2);
+            clk_tb <= '1';
+            wait for (PERIOD/2);
+    end process clk_generate;
+
     PROCESS IS
         -- CONSTANT DIVIDE : std_logic_vector(OP_WIDTH - 1 downto 0) := std_logic_vector(to_unsigned(2,OP_WIDTH));
         VARIABLE read_line : line; -- a buffer for what was read
         VARIABLE write_line : line; -- a buffer for what was written
-        FILE infile : text OPEN read_mode IS "divider32.in";
-        FILE outfile : text OPEN write_mode IS "divider32.out";
+        FILE infile : text OPEN read_mode IS "divider16.in";
+        FILE outfile : text OPEN write_mode IS "divider16.out";
         VARIABLE temp1 : INTEGER;
         VARIABLE temp2 : INTEGER;
 
     BEGIN
         wait for 1ns;
+        -- start_tb <= '1';
         
+        --have this accept clock
         WHILE NOT (endfile(infile)) LOOP
-            -- read in both operands and operations
-            readline(infile, read_line);
-            read(read_line, temp1);
-            dividend_tb <= STD_LOGIC_VECTOR(to_unsigned(temp1, DIVIDEND_WIDTH));
+            --read new input only if rising edge of start
+            -- if (rising_edge(start)) then 
+                -- read in both operands and operations
+                readline(infile, read_line);
+                read(read_line, temp1);
+                dividend_tb <= STD_LOGIC_VECTOR(to_unsigned(temp1, DIVIDEND_WIDTH));
 
-            readline(infile, read_line);
-            read(read_line, temp2);
-            divisor_tb <= STD_LOGIC_VECTOR(to_unsigned(temp2, DIVISOR_WIDTH));
+                readline(infile, read_line);
+                read(read_line, temp2);
+                divisor_tb <= STD_LOGIC_VECTOR(to_unsigned(temp2, DIVISOR_WIDTH));
 
-            wait for 1ns;            
-            start_tb <= '1';
+                -- wait for 1 ns;            
+                -- start_tb <= '1';
 
-            -- write operand one
-            write(write_line, temp1);
-            write(write_line, STRING'(" / "));
-            -- write operand two
-            write(write_line, temp2);
-            write(write_line, STRING'(" = "));
+                -- write operand one
+                write(write_line, temp1);
+                write(write_line, STRING'(" / "));
+                -- write operand two
+                write(write_line, temp2);
+                write(write_line, STRING'(" = "));
 
-            WAIT FOR 5 ns;
+                --or do you raise start here?
+                start_tb <= '1';
+            -- end if;
 
-            --calculator answer
+            --wait for computation
+            WAIT FOR PERIOD;
+
+             --calculator answer
             write(write_line, to_integer(unsigned(quotient_tb)));
             write(write_line, STRING'(" -- "));       
             write(write_line, to_integer(unsigned(remainder_tb)));
             writeline (outfile, write_line);
-            start_tb <= '0';
-            WAIT FOR 5 ns;
+            -- start_tb <= '0';
+            -- WAIT FOR PERIOD;
         END LOOP;
 
     END PROCESS;
