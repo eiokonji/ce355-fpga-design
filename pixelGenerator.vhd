6c404fad7685ffffbc4e27b087225acc6c1e9c80
unknown <ebubeokonji2024@u.northwestern.edu>
2023-11-20 13:20:15 -0600

    rendering bullets and tanks
    it works!

--- pixelGenerator.vhd
+++ pixelGenerator.vhd
@@ -5,11 +5,14 @@ use IEEE.numeric_std.all;
 
 entity pixelGenerator is
 	port(
-			clk, ROM_clk, rst_n, video_on, eof 				: in std_logic;
-			pixel_row, pixel_column						    : in std_logic_vector(9 downto 0);
-			A_lb, A_rb, A_tb, A_bb          : in std_logic_vector(9 downto 0);
-			B_lb, B_rb, B_tb, B_bb          : in std_logic_vector(9 downto 0);
-			red_out, green_out, blue_out					: out std_logic_vector(7 downto 0)
+			clk, ROM_clk, rst_n, video_on, eof 					: in std_logic;
+			pixel_row, pixel_column						    	: in std_logic_vector(9 downto 0);
+			tank_A_lb, tank_A_rb, tank_A_tb, tank_A_bb      	: in std_logic_vector(9 downto 0);
+			tank_B_lb, tank_B_rb, tank_B_tb, tank_B_bb      	: in std_logic_vector(9 downto 0);
+			bullet_A_lb, bullet_A_rb, bullet_A_tb, bullet_A_bb	: in std_logic_vector(9 downto 0);
+			bullet_B_lb, bullet_B_rb, bullet_B_tb, bullet_B_bb	: in std_logic_vector(9 downto 0);
+
+			red_out, green_out, blue_out						: out std_logic_vector(7 downto 0)
 		);
 end entity pixelGenerator;
 
@@ -37,8 +40,10 @@ signal colorAddress : std_logic_vector (2 downto 0);
 signal color        : std_logic_vector (23 downto 0);
 
 signal pixel_row_int, pixel_column_int : natural;
-signal A_lbound, A_rbound, A_tbound, A_bbound : natural;
-signal B_lbound, B_rbound, B_tbound, B_bbound : natural;
+signal tank_A_lbound, tank_A_rbound, tank_A_tbound, tank_A_bbound : natural;
+signal tank_B_lbound, tank_B_rbound, tank_B_tbound, tank_B_bbound : natural;
+signal bullet_A_lbound, bullet_A_rbound, bullet_A_tbound, bullet_A_bbound : natural;
+signal bullet_B_lbound, bullet_B_rbound, bullet_B_tbound, bullet_B_bbound : natural;
 
 begin
 
@@ -51,15 +56,25 @@ begin
 	pixel_row_int <= to_integer(unsigned(pixel_row));
 	pixel_column_int <= to_integer(unsigned(pixel_column));
 
-	A_lbound <= to_integer(unsigned(A_lb));
-	A_rbound <= to_integer(unsigned(A_rb));
-	A_tbound <= to_integer(unsigned(A_tb));
-	A_bbound <= to_integer(unsigned(A_bb));
+	tank_A_lbound <= to_integer(unsigned(tank_A_lb));
+	tank_A_rbound <= to_integer(unsigned(tank_A_rb));
+	tank_A_tbound <= to_integer(unsigned(tank_A_tb));
+	tank_A_bbound <= to_integer(unsigned(tank_A_bb));
 
-	B_lbound <= to_integer(unsigned(B_lb));
-	B_rbound <= to_integer(unsigned(B_rb));
-	B_tbound <= to_integer(unsigned(B_tb));
-	B_bbound <= to_integer(unsigned(B_bb));
+	tank_B_lbound <= to_integer(unsigned(tank_B_lb));
+	tank_B_rbound <= to_integer(unsigned(tank_B_rb));
+	tank_B_tbound <= to_integer(unsigned(tank_B_tb));
+	tank_B_bbound <= to_integer(unsigned(tank_B_bb));
+
+	bullet_A_lbound <= to_integer(unsigned(bullet_A_lb));
+	bullet_A_rbound <= to_integer(unsigned(bullet_A_rb)); 
+	bullet_A_tbound <= to_integer(unsigned(bullet_A_tb)); 
+	bullet_A_bbound <= to_integer(unsigned(bullet_A_bb));
+
+	bullet_B_lbound <= to_integer(unsigned(bullet_B_lb));
+	bullet_B_rbound <= to_integer(unsigned(bullet_B_rb)); 
+	bullet_B_tbound <= to_integer(unsigned(bullet_B_tb)); 
+	bullet_B_bbound <= to_integer(unsigned(bullet_B_bb));
 	
 --------------------------------------------------------------------------------------------	
 	
@@ -73,11 +88,18 @@ begin
 	begin
 			
 		if (rising_edge(clk)) then
-				if (pixel_row_int >= A_tbound and pixel_row_int < A_bbound and pixel_column_int >= A_lbound and pixel_column_int < A_rbound) then
+				if (pixel_row_int >= tank_A_tbound and pixel_row_int < tank_A_bbound and pixel_column_int >= tank_A_lbound and pixel_column_int < tank_A_rbound) then
                     colorAddress <= color_blue;
 
-                elsif (pixel_row_int >= B_tbound and pixel_row_int < B_bbound and pixel_column_int >= B_lbound and pixel_column_int < B_rbound) then
+                elsif (pixel_row_int >= tank_B_tbound and pixel_row_int < tank_B_bbound and pixel_column_int >= tank_B_lbound and pixel_column_int < tank_B_rbound) then
                     colorAddress <= color_red;
+
+				elsif (pixel_row_int >= bullet_A_tbound and pixel_row_int < bullet_A_bbound and pixel_column_int >= bullet_A_lbound and pixel_column_int < bullet_A_rbound) then
+                    colorAddress <= color_blue;
+
+                elsif (pixel_row_int >= bullet_B_tbound and pixel_row_int < bullet_B_bbound and pixel_column_int >= bullet_B_lbound and pixel_column_int < bullet_B_rbound) then
+                    colorAddress <= color_red;
+
 				else
 				    colorAddress <= color_white;
 
--- tank_game.vhd
+++ tank_game.vhd
@@ -1,37 +1,39 @@
 --This module controls the top-level VGA of the tank game
+
 LIBRARY IEEE;
 
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
-
-
 ENTITY tank_game IS
 	PORT (
 		CLOCK_50 : IN STD_LOGIC;
 		RESET_N : IN STD_LOGIC;
-
 		--VGA 
 		VGA_RED, VGA_GREEN, VGA_BLUE : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
 		HORIZ_SYNC, VERT_SYNC, VGA_BLANK, VGA_CLK : OUT STD_LOGIC
 
 	);
 END ENTITY tank_game;
+
 ARCHITECTURE structural OF tank_game IS
 	SIGNAL VGA_RED_temp, VGA_GREEN_temp, VGA_BLUE_temp : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
-	component clock_counter is 
-	port (
-		clk, rst_n : in std_logic;
-		game_tick : out std_logic
-	);
-	end component clock_counter;
+	COMPONENT clock_counter IS
+		PORT (
+			clk, rst_n : IN STD_LOGIC;
+			game_tick : OUT STD_LOGIC
+		);
+	END COMPONENT clock_counter;
 
 	COMPONENT pixelGenerator IS
 		PORT (
 			clk, ROM_clk, rst_n, video_on, eof : IN STD_LOGIC;
 			pixel_row, pixel_column : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-			A_lb, A_rb, A_tb, A_bb          : in std_logic_vector(9 downto 0);
-			B_lb, B_rb, B_tb, B_bb          : in std_logic_vector(9 downto 0);
+			tank_A_lb, tank_A_rb, tank_A_tb, tank_A_bb : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+			tank_B_lb, tank_B_rb, tank_B_tb, tank_B_bb : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+			bullet_A_lb, bullet_A_rb, bullet_A_tb, bullet_A_bb : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+			bullet_B_lb, bullet_B_rb, bullet_B_tb, bullet_B_bb : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+
 			red_out, green_out, blue_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
 		);
 	END COMPONENT pixelGenerator;
@@ -52,14 +54,30 @@ ARCHITECTURE structural OF tank_game IS
 		);
 	END COMPONENT tankB;
 
-	COMPONENT tankA_pos IS
-    PORT (
-        clk, rst_n, direction : IN STD_LOGIC;
-        speed : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        pos_x : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
-        updated_pos_x : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
-    );
-	END COMPONENT tankA_pos;
+	COMPONENT bulletA IS
+		PORT (
+			clk, rst_n : IN STD_LOGIC;
+			pos_x, pos_y : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+			x1, x2, y1, y2 : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+		);
+	END COMPONENT bulletA;
+
+	COMPONENT bulletB IS
+		PORT (
+			clk, rst_n : IN STD_LOGIC;
+			pos_x, pos_y : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+			x1, x2, y1, y2 : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+		);
+	END COMPONENT bulletB;
+
+	-- COMPONENT tankA_pos IS
+	-- PORT (
+	--     clk, rst_n, direction : IN STD_LOGIC;
+	--     speed : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+	--     pos_x : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+	--     updated_pos_x : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
+	-- );
+	-- END COMPONENT tankA_pos;
 
 	COMPONENT VGA_SYNC IS
 		PORT (
@@ -71,7 +89,7 @@ ARCHITECTURE structural OF tank_game IS
 	END COMPONENT VGA_SYNC;
 
 	--Signals for screen position updates
-	signal game_ticks : std_logic;
+	SIGNAL game_ticks : STD_LOGIC;
 
 	--Signals for VGA sync
 	SIGNAL pixel_row_int : STD_LOGIC_VECTOR(9 DOWNTO 0);
@@ -90,26 +108,46 @@ ARCHITECTURE structural OF tank_game IS
 	SIGNAL B_TOP_BOUND : STD_LOGIC_VECTOR(9 DOWNTO 0);
 	SIGNAL B_BOTTOM_BOUND : STD_LOGIC_VECTOR(9 DOWNTO 0);
 
-	--initial position of tank A is (435,280)
+	-- bullet positions
+	SIGNAL A_X1, A_X2, A_Y1, A_Y2 : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	SIGNAL B_X1, B_X2, B_Y1, B_Y2 : STD_LOGIC_VECTOR(9 DOWNTO 0);
+
+	--initial position of tank A is (280, 435)
 	SIGNAL A_POS_X : STD_LOGIC_VECTOR(9 DOWNTO 0) := (3 => '1', 4 => '1', 8 => '1', OTHERS => '0');
 	SIGNAL A_POS_Y : STD_LOGIC_VECTOR(9 DOWNTO 0) := (0 => '1', 1 => '1', 4 => '1', 5 => '1', 7 => '1', 8 => '1', OTHERS => '0');
-	SIGNAL A_POS_X_C : STD_LOGIC_VECTOR(9 downto 0);
-	SIGNAL A_POS_Y_C : STD_LOGIC_VECTOR(9 downto 0);
-	--initial position of tank B is (10,280)
+	SIGNAL A_POS_X_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	SIGNAL A_POS_Y_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
+
+	--initial position of tank B is (280, 10)
 	SIGNAL B_POS_X : STD_LOGIC_VECTOR(9 DOWNTO 0) := (3 => '1', 4 => '1', 8 => '1', OTHERS => '0');
 	SIGNAL B_POS_Y : STD_LOGIC_VECTOR(9 DOWNTO 0) := (1 => '1', 3 => '1', OTHERS => '0');
-	SIGNAL B_POS_X_C : STD_LOGIC_VECTOR(9 downto 0);
-	SIGNAL B_POS_Y_C : STD_LOGIC_VECTOR(9 downto 0);
+	SIGNAL B_POS_X_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	SIGNAL B_POS_Y_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
+
+	-- bullet position signals, bullet A (315, 410)
+	-- (x,y) := (tank_X + 15, tank_X - 25)
+	-- _C is clocked signal
+	SIGNAL A_BULLET_POS_X : STD_LOGIC_VECTOR(9 DOWNTO 0) := (8 => '1', 5 => '1', 4 => '1', 3 => '1', 1 => '1', 0 => '1',OTHERS => '0');
+	SIGNAL A_BULLET_POS_Y : STD_LOGIC_VECTOR(9 DOWNTO 0) := (8 => '1', 7 => '1', 4 => '1', 3 => '1', 1 => '1', OTHERS => '0');
+	SIGNAL A_BULLET_POS_X_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	SIGNAL A_BULLET_POS_Y_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
+
+	-- bullet position signals, bullet B (315, 50)
+	-- (x,y) := (tank_X + 15, tank_X + 40)
+	SIGNAL B_BULLET_POS_X : STD_LOGIC_VECTOR(9 DOWNTO 0) := (8 => '1', 5 => '1', 4 => '1', 3 => '1', 1 => '1', 0 => '1',OTHERS => '0');
+	SIGNAL B_BULLET_POS_Y : STD_LOGIC_VECTOR(9 DOWNTO 0) := (5 => '1', 4 => '1', 1 => '1', OTHERS => '0');
+	SIGNAL B_BULLET_POS_X_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	SIGNAL B_BULLET_POS_Y_C : STD_LOGIC_VECTOR(9 DOWNTO 0);
 
 BEGIN
 
 	--------------------------------------------------------------------------------------------
-	clockCount : clock_counter
-	port map (
-		clk => CLOCK_50, 
-		rst_n => RESET_N,
-        game_tick => game_ticks
-	);
+	-- clockCount : clock_counter
+	-- PORT MAP(
+	-- 	clk => CLOCK_50,
+	-- 	rst_n => RESET_N,
+	-- 	game_tick => game_ticks
+	-- );
 
 	videoGen : pixelGenerator
 	PORT MAP(
@@ -120,14 +158,23 @@ BEGIN
 		eof => eof,
 		pixel_row => pixel_row_int,
 		pixel_column => pixel_column_int,
-		A_lb => A_LEFT_BOUND, 
-		A_rb => A_RIGHT_BOUND, 
-		A_tb => A_TOP_BOUND, 
-		A_bb => A_BOTTOM_BOUND, 
-		B_lb => B_LEFT_BOUND, 
-		B_rb => B_RIGHT_BOUND, 
-		B_tb => B_TOP_BOUND, 
-		B_bb => B_BOTTOM_BOUND,          
+
+		tank_A_lb => A_LEFT_BOUND,
+		tank_A_rb => A_RIGHT_BOUND,
+		tank_A_tb => A_TOP_BOUND,
+		tank_A_bb => A_BOTTOM_BOUND,
+		tank_B_lb => B_LEFT_BOUND,
+		tank_B_rb => B_RIGHT_BOUND,
+		tank_B_tb => B_TOP_BOUND,
+		tank_B_bb => B_BOTTOM_BOUND,
+		bullet_A_lb => A_X1,
+		bullet_A_rb => A_X2,
+		bullet_A_tb => A_Y1,
+		bullet_A_bb => A_Y2,
+		bullet_B_lb => B_X1,
+		bullet_B_rb => B_X2,
+		bullet_B_tb => B_Y1,
+		bullet_B_bb => B_Y2,
 		red_out => VGA_RED,
 		green_out => VGA_GREEN,
 		blue_out => VGA_BLUE
@@ -143,7 +190,7 @@ BEGIN
 		right_bound => A_RIGHT_BOUND,
 		top_bound => A_TOP_BOUND,
 		bottom_bound => A_BOTTOM_BOUND
-	);	
+	);
 
 	tankB_Draw : tankB
 	PORT MAP(
@@ -156,16 +203,40 @@ BEGIN
 		top_bound => B_TOP_BOUND,
 		bottom_bound => B_BOTTOM_BOUND
 	);
-	
-	tankA_Position : tankA_pos
-    PORT MAP(
-        clk => game_ticks, 
-		rst_n => RESET_N, 
-		direction => A_DIRECTION,
-        speed => A_SPEED,
-        pos_x => A_POS_X,
-        updated_pos_x => A_POS_X_C
-    );
+
+	bulletA_pos : bulletA
+	PORT MAP(
+		clk => CLOCK_50,
+		rst_n => RESET_N,
+		pos_x => A_BULLET_POS_X,
+		pos_y => A_BULLET_POS_Y,
+		x1 => A_X1,
+		x2 => A_X2,
+		y1 => A_Y1,
+		y2 => A_Y2
+	);
+
+	bulletB_pos : bulletB
+	PORT MAP(
+		clk => CLOCK_50,
+		rst_n => RESET_N,
+		pos_x => B_BULLET_POS_X,
+		pos_y => B_BULLET_POS_Y,
+		x1 => B_X1,
+		x2 => B_X2,
+		y1 => B_Y1,
+		y2 => B_Y2
+	);
+
+	-- tankA_Position : tankA_pos
+	-- PORT MAP(
+	-- 	clk => game_ticks,
+	-- 	rst_n => RESET_N,
+	-- 	direction => A_DIRECTION,
+	-- 	speed => A_SPEED,
+	-- 	pos_x => A_POS_X,
+	-- 	updated_pos_x => A_POS_X_C
+	-- );
 
 	--------------------------------------------------------------------------------------------
 	--This section should not be modified in your design.  This section handles the VGA timing signals
